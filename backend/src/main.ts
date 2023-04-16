96a34d7c8da67117a4efb8df83f7ffbcdab62249
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2023-04-16 21:19:53 +0300

    добавить хост удаленного сервера

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import {AppModule} from "./app.module";
 
 async function start() {
   const PORT = process.env.PORT ?? 5000;
-  const HOST = process.env.HOST ?? 'frelance-admin';
+  const HOST = process.env.HOST ?? '194.67.116.54';
   const app = await NestFactory.create(AppModule)
 
   app.listen(PORT, HOST).then(() => console.log(`server starts on port ${PORT}`))
