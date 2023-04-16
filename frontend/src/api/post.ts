e19134ca07e084fc727b5b0e26eb14dad7e72780
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2023-04-16 20:47:12 +0300

    fix

--- frontend/src/api/post.ts
+++ frontend/src/api/post.ts
@@ -1,3 +1,6 @@
-export const VALIDATE_REGISTRATION_URI = 'http://frelance-admin/:7000/api/auth/registration'
-export const CONFIRM_REGISTRATION_URI = 'http://frelance-admin/:7000/api/auth/registration-confirm'
-export const LOGIN_URI = 'http://frelance-admin/:7000/api/auth/login'
+const host = window.location.host;
+const protocol = window.location.protocol;
+
+export const VALIDATE_REGISTRATION_URI = `${protocol}//${host}/api/auth/registration`
+export const CONFIRM_REGISTRATION_URI = `${protocol}//${host}/api/auth/registration-confirm`
+export const LOGIN_URI = `${protocol}//${host}/api/auth/login`
