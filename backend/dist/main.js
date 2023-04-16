9eb467690143a5d3de3e6b437aa516d91e91d896
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2023-04-16 21:20:06 +0300

    билд

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -5,7 +5,7 @@ const app_module_1 = require("./app.module");
 async function start() {
     var _a, _b;
     const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5000;
-    const HOST = (_b = process.env.HOST) !== null && _b !== void 0 ? _b : 'frelance-admin';
+    const HOST = (_b = process.env.HOST) !== null && _b !== void 0 ? _b : '194.67.116.54';
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.listen(PORT, HOST).then(() => console.log(`server starts on port ${PORT}`));
 }
