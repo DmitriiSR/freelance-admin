c47f0ffb4eb072dce2165c38b54ee51291696b5f
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2023-04-16 21:24:32 +0300

    подключить бд

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function start() {
     var _a, _b;
-    const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5000;
+    const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 80;
     const HOST = (_b = process.env.HOST) !== null && _b !== void 0 ? _b : '194.67.116.54';
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.listen(PORT, HOST).then(() => console.log(`server starts on port ${PORT}`));
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 
 async function start() {
-  const PORT = process.env.PORT ?? 5000;
+  const PORT = process.env.PORT ?? 80;
   const HOST = process.env.HOST ?? '194.67.116.54';
   const app = await NestFactory.create(AppModule)
 
