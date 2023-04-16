22a7e7743aaa4ce02f0a49efe73bb22595020610
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2023-04-16 21:11:00 +0300

    отключить бд для теста

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import {Module} from "@nestjs/common";
-import {SequelizeModule} from "@nestjs/sequelize";
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { User } from './users/users.model';
-import { AuthModule } from './auth/auth.module';
-import { ConfirmCodeModule } from './confirm_code/confirm_code.module';
+// import {SequelizeModule} from "@nestjs/sequelize";
+// import { UsersModule } from './users/users.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { User } from './users/users.model';
+// import { AuthModule } from './auth/auth.module';
+// import { ConfirmCodeModule } from './confirm_code/confirm_code.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { BaseExceptionFilter } from '@nestjs/core';
 import * as path from 'path';
@@ -18,22 +18,22 @@ import * as path from 'path';
         },
     ],
     imports: [
-        ConfigModule.forRoot({
-            envFilePath: `.${process.env.NODE_ENV}.env`
-        }),
-        SequelizeModule.forRoot({
-            dialect: 'postgres',
-            host: process.env.POSTGRES_HOST,
-            port: Number(process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB,
-            models: [User],
-            autoLoadModels: true
-        }),
-        UsersModule,
-        AuthModule,
-        ConfirmCodeModule,
+        // ConfigModule.forRoot({
+        //     envFilePath: `.${process.env.NODE_ENV}.env`
+        // }),
+        // SequelizeModule.forRoot({
+        //     dialect: 'postgres',
+        //     host: process.env.POSTGRES_HOST,
+        //     port: Number(process.env.POSTGRES_PORT),
+        //     username: process.env.POSTGRES_USER,
+        //     password: process.env.POSTGRES_PASSWORD,
+        //     database: process.env.POSTGRES_DB,
+        //     models: [User],
+        //     autoLoadModels: true
+        // }),
+        // UsersModule,
+        // AuthModule,
+        // ConfirmCodeModule,
         ServeStaticModule.forRoot({
             rootPath: path.join(__dirname, '../../', 'frontend', 'dist'),
             exclude: ['/api*'], // исключаем все маршруты, начинающиеся с "/api"
